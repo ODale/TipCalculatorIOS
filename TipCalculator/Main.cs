0461562bc7bcbbd2b42ea9ec7fcfed6513d719f3
O'Dale Cotterell <ocotterell@gmail.com>
2017-09-13 01:25:27 -0400

    testing update to mac osx

--- TipCalculator/Main.cs
+++ TipCalculator/Main.cs
@@ -9,6 +9,9 @@ namespace TipCalculator
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            
+            //testing update
+
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
